31bc511e32c640d4f66145c6419decbd1c96aa32
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-16 00:22:31 +0530

    First commit

---
+++ First.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+       //variable declare 4 bytes
+    //   int a = 10, b = 20, c = 30,d, e =11;
+    //   cout<<a+c;
+    
+    //   char c = 'a';
+    //   cout<<'a';
+
+    //   float f = 2.31;
+    //   cout<<f;
+
+    //   double d = 3.121;
+    //   cout<<d;
+
+    bool b = false;
+    cout<<b;
+
+
+}
+       
+
+
+
