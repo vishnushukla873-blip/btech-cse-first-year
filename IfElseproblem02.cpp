cdae0a2f8dc596ec969df33dcdada602aecbf342
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-17 11:40:54 +0530

    Add IfElseproblem02.cpp

---
+++ IfElseproblem02.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number%2==0)
+    {
+        cout<<"even number";
+    }
+    else
+    {
+        cout<<"odd number";
+    }
+
+
+
+
+
+
+
+}
