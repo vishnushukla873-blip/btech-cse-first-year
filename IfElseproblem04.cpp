c78d269e09c74b8f4157227cbc023c77dd01dc18
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-17 11:43:38 +0530

    Add IfElseproblem04.cpp

---
+++ IfElseproblem04.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number>0)
+    {
+        cout<<"positive number";
+    }
+    else if(number==0)
+    {
+        cout<<"zero";
+    }
+    else
+    {
+        cout<<"negative number";
+    }
+
+
+
+
+
+}
