5e6a960b70b002b4eff44461317f826429bfb38f
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-17 11:42:31 +0530

    Add IfElseproblem03.cpp

---
+++ IfElseproblem03.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    if(age>=18)
+    {
+        cout<<"eligibility to vote";
+    }
+    else
+    {
+        cout<<"not have eligibility to vote";
+    }
+
+
+
+
+
+
+
+
+}
