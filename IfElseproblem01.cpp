ceda75186ad1810f55b6d99c0a60885ce6183847
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-17 11:39:45 +0530

    Add IfElseproblemnumber01.cpp

---
+++ IfElseproblem01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int package;
+    cin>>package;
+    if(package>10)
+    {
+        cout<<"accpted";
+    }
+    else
+    {
+        cout<<"not accepted";
+    }
+
+
+
+
+
+
+
+}
