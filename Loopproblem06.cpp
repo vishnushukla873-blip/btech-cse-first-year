212053c23ff75a9e636367dad54f15d3e776329a
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-20 21:30:52 +0530

    Add loop problem solving

---
+++ Loopproblem06.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter the number from 1 to 100"<<endl;
+    for(i=1; i<=100; i=i+3)
+    {
+        cout<<i<<endl;
+        }    }
