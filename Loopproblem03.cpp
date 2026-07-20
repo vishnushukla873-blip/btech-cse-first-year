1705731200fe5c86d3a09ff91a9a0057c00f03c1
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-20 21:28:22 +0530

    Add loop problem solving

---
+++ Loopproblem03.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    
+    for(int i = 2; i<=n ; i=i+2)
+    {
+        cout<<i<<endl;
+    }
+
+
+
+}
