082842a47490571f7c3edd244429b74579df925b
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-21 09:58:59 +0530

    Add loop problem solving

---
+++ Loopproblem07.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    cout<<"Enter the table"<<endl;
+    cin>>n;
+    
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    }
+
+
+
+
+
+}
