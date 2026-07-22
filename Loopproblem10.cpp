687b184bd088cb16f4df5eb8f80e56f6af6dee9b
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-22 19:10:53 +0530

    Add loop problem solving

---
+++ Loopproblem10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i, n;
+cout<<"Enter the number";
+cin>>n;
+
+int fact = 1;
+ 
+for(i=1;i<=n;i=i+1)
+{
+    fact = fact * i;
+}
+cout<<n<<"!="<<fact;fact;
+return 0;
+
+}
