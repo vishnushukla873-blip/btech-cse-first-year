b62c36ced4ebb8cb591b984ac87724e7ec258634
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-24 11:09:08 +0530

    solve loops problem by using patternprinting

---
+++ PatternPrinting14.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row , col;
+   for(row=1;row<=8;row=row+1)
+   {
+     for(col=1;col<=row;col=col+1)
+     {
+          cout<<row<<" ";
+     }
+     cout<<endl;
+   }
+     return 0 ;
+
+
+
+
+
+
+
+
+
+}
