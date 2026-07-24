fa7bbbef479db526661397b4b2ffd92547a5fea0
Vishnu Shukla <vishnushukla873@gmail.com>
2026-07-24 11:10:07 +0530

    Add loop problem using patternprinting

---
+++ PatternPrinting15.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int row , col ;
+  for(row=1;row<=7;row=row+1)
+  {
+    for(col=row;col>=1;col=col-1)
+    {
+         cout<<col<<" ";
+
+
+    }
+           cout<<endl;
+
+  }
+return 0 ;
+
+
+
+
+
+
+
+
+
+
+}
